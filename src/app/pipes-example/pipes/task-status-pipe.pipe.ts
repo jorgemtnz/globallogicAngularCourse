f6ace0df088159038a5c0c14338f04cbb02f7634
jmartinez@linq.com <jmartinez@linq.com>
2024-02-14 17:22:28 -0300

    add comment to ask

--- src/app/pipes-example/pipes/task-status-pipe.pipe.ts
+++ src/app/pipes-example/pipes/task-status-pipe.pipe.ts
@@ -5,8 +5,8 @@ import { ITaskModel } from '../model/itask-model';
   name: 'taskStatusPipe'
 })
 export class TaskStatusPipePipe implements PipeTransform {
-
-  transform(task: ITaskModel, ...args: unknown[]): unknown {
+//evalua una sola vez la directiva, no cada vez que se presiona el boton y cambia el status, por eso antes no cambiaba nunca el valor.
+  transform(task: ITaskModel): string {
     return task.title.toUpperCase();
   }
 
